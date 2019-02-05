6d6dc8c1ed559c743966059dbeaec8762a7e4a15
EC2 Default User <ec2-user@ip-172-31-20-17.us-east-2.compute.internal>
2019-02-05 22:42:16 +0000

    First commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,63 @@
 # When done, submit this entire file to the autograder.
 
+# Naresh Choudhary
+#UIN: 427008954
+
 # Part 1
 
 def sum arr
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return arr.sort.last(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.combination(2).any? {|a, b| (a + b) == n}
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.empty?
+  	return false
+  elsif ( /[a-z]/ =~ s[0].downcase && /[^aeiou]/ =~ s[0].downcase)
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # YOUR CODE HEREif 
+  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+		return true
+	else
+		return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
